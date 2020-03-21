4adfce9363a1aca9a45f41f04ece8c897e2f5118
tomjmcnamee <tom@tomjmcnamee.com>
2020-03-21 19:03:44 -0400

    get to patch

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
       
       get '/processed_stats', to: "processed_stat#index"
       get '/states', to: "state#index"
-      get '/daily_5p_update', to: "db_update#Daily5pUpdate"
+      patch '/daily_5p_update', to: "db_update#Daily5pUpdate"
       patch '/bulk_load_arr_of_dates', to: "db_update#BulkLoadDatesData"
       # patch '/bulk_load_arr_of_dates/:dates', to: "db_update#BulkLoadDatesData"
      
