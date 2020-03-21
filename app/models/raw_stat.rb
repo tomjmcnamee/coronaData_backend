4f99d3034c32b18aa6120885634582d36f39fd33
tomjmcnamee <tom@tomjmcnamee.com>
2020-03-21 11:54:08 -0400

    updated seeds to include states

--- app/models/raw_stat.rb
+++ app/models/raw_stat.rb
@@ -11,8 +11,6 @@ class RawStat < ApplicationRecord
   end 
 
   def self.pullAndProcessDaysData(arrOfDatesToProcess)
-    require 'nokogiri'
-    require 'open-uri'
     for d in arrOfDatesToProcess
       jsonData = JSON.load(open("https://covidtracking.com/api/states/daily?date=#{d}"))
       ##Inserts ALL data into the Stats table.
