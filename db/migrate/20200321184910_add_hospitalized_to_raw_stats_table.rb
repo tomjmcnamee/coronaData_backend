e62ac95ccb55f3686297a50fbb9092fd4f3e6d4c
tomjmcnamee <tom@tomjmcnamee.com>
2020-03-21 14:51:13 -0400

    new migration fixed

--- db/migrate/20200321184910_add_hospitalized_to_raw_stats_table.rb
+++ db/migrate/20200321184910_add_hospitalized_to_raw_stats_table.rb
@@ -1,5 +1,5 @@
 class AddHospitalizedToRawStatsTable < ActiveRecord::Migration[6.0]
   def change
-    add_column :raw_stats_tables, :hospitalized, :string
+    add_column :raw_stats, :hospitalized, :string
   end
 end
