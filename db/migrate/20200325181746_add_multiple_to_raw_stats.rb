72792cad31f04b779ad90f30bd44f7360dfbd0fc
tomjmcnamee <tom@tomjmcnamee.com>
2020-03-25 17:58:40 -0400

    chasing json data

---
+++ db/migrate/20200325181746_add_multiple_to_raw_stats.rb
@@ -0,0 +1,9 @@
+class AddTotalTestResultsToRawStats < ActiveRecord::Migration[6.0]
+  def change
+    add_column :raw_stats, :deathIncrease, :integer
+    add_column :raw_stats, :hospitalizedIncrease, :integer
+    add_column :raw_stats, :negativeIncrease, :integer
+    add_column :raw_stats, :positiveIncrease, :integer
+    add_column :raw_stats, :totalTestResultsIncrease, :integer
+  end
+end
