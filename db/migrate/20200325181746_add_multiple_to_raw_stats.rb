4106a33e6c66fb527367ff3de47e694a295e94b6
tomjmcnamee <tom@tomjmcnamee.com>
2020-03-25 18:06:51 -0400

    ajnothet chase

--- db/migrate/20200325181746_add_multiple_to_raw_stats.rb
+++ db/migrate/20200325181746_add_multiple_to_raw_stats.rb
@@ -1,4 +1,4 @@
-class AddTotalTestResultsToRawStats < ActiveRecord::Migration[6.0]
+class AddMultipleToRawStats < ActiveRecord::Migration[6.0]
   def change
     add_column :raw_stats, :deathIncrease, :integer
     add_column :raw_stats, :hospitalizedIncrease, :integer
