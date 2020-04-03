50970a5ec2029f6de056d5e9a52ecd3cf93ada5f
tomjmcnamee <tom@tomjmcnamee.com>
2020-04-03 01:25:39 -0400

    changed Order Action on SAHO stable

--- db/migrate/20200402034005_create_stay_at_home_orders.rb
+++ db/migrate/20200402034005_create_stay_at_home_orders.rb
@@ -3,7 +3,7 @@ class CreateStayAtHomeOrders < ActiveRecord::Migration[6.0]
     create_table :stay_at_home_orders do |t|
       t.integer :date
       t.references :state, null: false, foreign_key: true
-      t.string :type
+      t.string :order_action
 
       t.timestamps
     end
