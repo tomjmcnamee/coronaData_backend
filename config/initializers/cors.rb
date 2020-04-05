f49df2b83c3d291f1730ca4effa2412e290b73ad
tomjmcnamee <tom@tomjmcnamee.com>
2020-04-05 11:51:52 -0400

    added my public IP as cors Env var

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,10 +8,19 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # origins '*'
-    origins ENV["CORS_ORIGINS"]
+    origins ENV["CORS_TJM_ORIGINS"]
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: :get
+  end
+
+  allow do
+    # origins '*'
+    origins ENV["CORS_PUBLICIP_ORIGINS"]
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :patch]
   end
 end
